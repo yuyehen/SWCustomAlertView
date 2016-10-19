07e4c580677d15127ff8248482714fa0b311220f
sunwei <282547935@qq.com>
2016-10-19 15:59:44 +0800

    使用方法

--- SWCustomAlertView/ViewController.m
+++ SWCustomAlertView/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SWCustomAlertView.h"
 @interface ViewController ()
 
 @end
@@ -16,7 +16,69 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+- (IBAction)oneAlert:(id)sender {
+    SWCustomAlertView *alert = [[SWCustomAlertView alloc] initWithTitle:@"一个弹框" message:@"1" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    [alert show];
+}
+- (IBAction)twoAlert:(id)sender {
+    SWCustomAlertView *alert = [[SWCustomAlertView alloc] initWithTitle:@"第一个弹框" message:@"1" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    [alert show];
+    
+    SWCustomAlertView *alert1 = [[SWCustomAlertView alloc] initWithTitle:@"第二个弹框" message:@"2" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    [alert1 show];
+}
+- (IBAction)onlyOneAlert:(id)sender {
+    SWCustomAlertView *alert = [[SWCustomAlertView alloc] initWithTitle:@"第一个弹框" message:@"1" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    alert.isOnlyOne = YES;
+    [alert show];
+    
+    SWCustomAlertView *alert1 = [[SWCustomAlertView alloc] initWithTitle:@"第二个弹框" message:@"2" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    alert1.isOnlyOne = YES;
+    [alert1 show];
+}
+- (IBAction)moreAlert:(id)sender {
+    SWCustomAlertView *alert = [[SWCustomAlertView alloc] initWithTitle:@"第一个弹框" message:@"1" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    alert.isOnlyOne = YES;
+    [alert show];
+    
+    SWCustomAlertView *alert1 = [[SWCustomAlertView alloc] initWithTitle:@"第二个弹框" message:@"2" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    alert1.isOnlyOne = YES;
+    [alert1 show];
+    SWCustomAlertView *alert2 = [[SWCustomAlertView alloc] initWithTitle:@"第三个弹框" message:@"3" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    [alert2 show];
+}
+
+
+- (IBAction)notDisappear:(id)sender {
+    SWCustomAlertView *alert = [[SWCustomAlertView alloc] initWithTitle:@"第一个弹框" message:@"1" cancelButtonTitle:@"取消" otherButtonTitles:@[@"确定"] actionBlock:^(NSInteger buttonIndex) {
+        
+    }];
+    alert.alwayDisplay = YES;
+    [alert show];
+    
+    [self performSelector:@selector(close:) withObject:alert afterDelay:5.0];
+}
+
+- (void)close:(SWCustomAlertView *)alert {
+    [alert dismiss];
 }
 
 - (void)didReceiveMemoryWarning {
